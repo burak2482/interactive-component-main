c32cb2af1005bd4f87028bd20316bbeb1113266d
Burak Durmuşoğlu <108025275+burak2482@users.noreply.github.com>
2024-10-12 10:53:17 +0300

    Update page.js

--- app/page.js
+++ app/page.js
@@ -7,28 +7,33 @@ export default function Home() {
   const [isSubmitted, setSubmitted] = useState(false);
   const [currentStep, setCurrentStep] = useState(1);
 
-
   return (
     <>
-    {isSubmitted ? (<main class='flex flex-col justify-center items-center min-h-svh bg-customVeryDarkBlue'>
-        <section class='bg-customDarkBlue mobile:w-1/4 mobile:h-1/4 px-6 py-7 rounded-3xl flex flex-col items-center justify-center'>
-        <Image src='illustration-thank-you.svg' class='flex items-center justify-center' width={200} height={200} alt=''/>
-        <h2 class='px-7 py-2 bg-slate-600 text-customOrange text-bold rounded-3xl mt-3'>You selected {currentStep} out of 5</h2>
-        <h1 class='text-white text-3xl font-semibold mt-5'>Thank you!</h1>
-        <h2 class='text-customGrey text-xl font-medium mt-5'>We appreciate you taking the time to give a rating. If you ever need more support don't hesitate to get in touch!</h2>
-        </section>
-      </main>) : (
-      <main class='flex flex-col justify-center items-center min-h-svh bg-customVeryDarkBlue'>
-      <section class='bg-customDarkBlue mobile:w-1/4 mobile:h-1/4 px-6 py-7 rounded-3xl'>
-        <Image src='icon-star.svg' class='px-2 py-3 bg-slate-600 rounded-full' width={50} height={50} alt=''/>
-        <h1 class='text-white text-3xl font-semibold mt-5'>How did we do?</h1>
-        <h2 class='text-customGrey text-xl font-medium mt-5'>Please let us know how we did with your support request. All feedback is appreciated to help us improve our offering!</h2>
-        <div class='mt-7'>
-          <Step currentStep={currentStep} setCurrentStep={setCurrentStep} isSubmitted={isSubmitted} setSubmitted={setSubmitted}  />
-        </div>
-      </section>
-    </main>
-    )}
+      {isSubmitted ? (
+        <main className='flex flex-col justify-center items-center min-h-svh bg-customVeryDarkBlue'>
+          <section className='bg-customDarkBlue mobile:w-1/4 mobile:h-1/4 px-6 py-7 rounded-3xl flex flex-col items-center justify-center'>
+            <Image src='illustration-thank-you.svg' className='flex items-center justify-center' width={200} height={200} alt='' />
+            <h2 className='px-7 py-2 bg-slate-600 text-customOrange text-bold rounded-3xl mt-3'>You selected {currentStep} out of 5</h2>
+            <h1 className='text-white text-3xl font-semibold mt-5'>Thank you!</h1>
+            <h2 className='text-customGrey text-xl font-medium mt-5'>
+              We appreciate you taking the time to give a rating. If you ever need more support don&apos;t hesitate to get in touch!
+            </h2>
+          </section>
+        </main>
+      ) : (
+        <main className='flex flex-col justify-center items-center min-h-svh bg-customVeryDarkBlue'>
+          <section className='bg-customDarkBlue mobile:w-1/4 mobile:h-1/4 px-6 py-7 rounded-3xl'>
+            <Image src='icon-star.svg' className='px-2 py-3 bg-slate-600 rounded-full' width={50} height={50} alt='' />
+            <h1 className='text-white text-3xl font-semibold mt-5'>How did we do?</h1>
+            <h2 className='text-customGrey text-xl font-medium mt-5'>
+              Please let us know how we did with your support request. All feedback is appreciated to help us improve our offering!
+            </h2>
+            <div className='mt-7'>
+              <Step currentStep={currentStep} setCurrentStep={setCurrentStep} isSubmitted={isSubmitted} setSubmitted={setSubmitted} />
+            </div>
+          </section>
+        </main>
+      )}
     </>
   );
 }
