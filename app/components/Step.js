c9e9ba550f63b15dbc42d17699b801ac139f3aad
Burak Durmuşoğlu <108025275+burak2482@users.noreply.github.com>
2024-10-12 10:58:00 +0300

    Update Step.js

--- app/components/Step.js
+++ app/components/Step.js
@@ -13,13 +13,22 @@ export default function Step({ isSubmitted, setSubmitted, currentStep, setCurren
   return (
     <>
       <section className="flex justify-between items-center">
-        <div className={`px-6 py-3 w-1 ${currentStep === 4 ? 'bg-customOrange' : (currentStep - 1 === 4 ? 'bg-white text-black' : 'bg-slate-600 text-white')} rounded-full flex justify-center items-center text-semibold`}>
+        <div className={`px-6 py-3 ${currentStep === 1 ? "bg-customOrange" : (currentStep - 1 === 1 ? "bg-white text-black" : "bg-slate-600 text-white")} rounded-full flex justify-center items-center text-semibold`}>
+          1
+        </div>
+        <div className={`px-6 py-3 ${currentStep === 2 ? "bg-customOrange" : (currentStep - 1 === 2 ? "bg-white text-black" : "bg-slate-600 text-white")} rounded-full flex justify-center items-center text-semibold`}>
+          2
+        </div>
+        <div className={`px-6 py-3 ${currentStep === 3 ? "bg-customOrange" : (currentStep - 1 === 3 ? "bg-white text-black" : "bg-slate-600 text-white")} rounded-full flex justify-center items-center text-semibold`}>
+          3
+        </div>
+        <div className={`px-6 py-3 ${currentStep === 4 ? "bg-customOrange" : (currentStep - 1 === 4 ? "bg-white text-black" : "bg-slate-600 text-white")} rounded-full flex justify-center items-center text-semibold`}>
           4
         </div>
-        <div className={`px-6 py-3 w-1 ${currentStep === 5 ? 'bg-customOrange' : (currentStep - 1 === 5 ? 'bg-white text-black' : 'bg-slate-600 text-white')} rounded-full flex justify-center items-center text-semibold`}>
+        <div className={`px-6 py-3 ${currentStep === 5 ? "bg-customOrange" : (currentStep - 1 === 5 ? "bg-white text-black" : "bg-slate-600 text-white")} rounded-full flex justify-center items-center text-semibold`}>
           5
         </div>
-      </section>
+    </section>
       <div className="flex items-center justify-center mt-8">
         <button onClick={handleStepAdder} className="bg-white text-bold text-2xl px-14 rounded-3xl mr-3 hover:bg-customGrey hover:text-white">
           NEXT
